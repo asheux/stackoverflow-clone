eaf59fb444da56dc93789eadc3074d6b80714536
asheuh <asheuh49@gmail.com>
2018-08-15 13:56:03 +0300

    #159757959 setup the database

--- stackoverflow/.travis.sh
+++ stackoverflow/.travis.sh
@@ -1,4 +1,5 @@
-export JWT_SECRET_KEY='i love hot ladies'
+export DATABASE_URL="postgresql+psycopg2://asheuh:@mermaid@localhost:5432/stackoverflow_test"
 export ENVIRONMENT="DEVELOPMENT"
 export FLASK_DEBUG = 1
+export TEST_DATABASE_URI="postgresql+psycopg2://postgres:@bashtech1234@localhost:5432/travis_test"
 export FLASK_APP='app.py'
--- stackoverflow/__init__.py
+++ stackoverflow/__init__.py
@@ -1,15 +1,52 @@
 import logging.config
 import os
-from flask import Flask
+from urllib.parse import urlparse
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from flask_restplus import Api
+from flask import Flask, Blueprint
 from flask_jwt_extended import JWTManager
 from stackoverflow import settings
-from stackoverflow.api.restplus import blueprint, api
+
+from stackoverflow.api.restplus import blueprint, api, authorizations
 from stackoverflow.api.v1.auth.routes.routes import ns as user_namespace
 from stackoverflow.api.v1.questions.routes import ns as question_namespace
 
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
 logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
+
+class Database:
+    connection = None
+    cursor = None
+    app = None
+
+    def init_db(self, app):
+        self.app = app
+        self.url = urlparse(os.environ["DATABASE_URL"])
+        self.connection = psycopg2.connect(
+            dbname=self.url.path[1:],
+            user=self.url.username,
+            password=self.url.password,
+            host=self.url.hostname,
+            port=self.url.port
+        )
+        self.cursor = self.connection.cursor(cursor_factory=RealDictCursor)
+
+v2_db = Database()
+v2_blueprint = Blueprint('api_v2', __name__, url_prefix='/api/v2')
+v2_api = Api(v2_blueprint, authorizations=authorizations, version='1.1', title='V2 of stackoverflow-lite questions API',
+             description=(
+                 "This is an api for StackOverflow-lite platform where people can ask questions and provide answers.\n\n"
+                 "##Exploring the demo.\n"
+                 "Create a new user at the 'POST /auth/register' endpoint. Get the user access token from the response."
+                 "Click the authorize button and add the token in the following format.\n\n"
+                 "`Bearer (jwt-token without the brackets)`\n\n"
+
+                 "## Authorization token (using)\n"
+                 "`Jwt-Extended`"
+             ),
+        )
 
 def configure_app(flask_app):
     flask_app.config['SWAGGER_UI_DOC_EXPANSION'] = settings.RESTPLUS_SWAGGER_UI_DOC_EXPANSION
@@ -20,6 +57,7 @@ def configure_app(flask_app):
     flask_app.config['JWT_BLACKLIST_ENABLED'] = settings.JWT_BLACKLIST_ENABLED
     flask_app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = settings.JWT_BLACKLIST_TOKEN_CHECKS
     flask_app.config['TESTING'] = settings.TESTING
+    flask_app.config['DATABASE_URL'] = settings.DATABASE_URL
 
 def initialize_app(flask_app):
     configure_app(flask_app)
@@ -32,7 +70,10 @@ def initialize_app(flask_app):
         return BlackListToken.check_blacklist(jti)
 
     jwt._set_error_handler_callbacks(api)
+    jwt._set_error_handler_callbacks(v2_api)
+    flask_app.register_blueprint(v2_blueprint)
     flask_app.register_blueprint(blueprint)
+    v2_db.init_db(flask_app)
 
 def create_app(config_name):
     app = Flask(__name__)
