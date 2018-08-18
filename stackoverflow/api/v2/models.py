34e5a20e07200a5a5425f3698828eed2e69925c1
asheuh <asheuh49@gmail.com>
2018-08-18 18:11:36 +0300

    Fix heroku deployment error

--- stackoverflow/api/v2/models.py
+++ stackoverflow/api/v2/models.py
@@ -36,19 +36,25 @@ class User(User, DatabaseCollector):
     @classmethod
     def get_all(cls):
         """Get all the items in the database"""
-        v2_db.cursor.execute("SELECT * FROM users")
-        users = v2_db.cursor.fetchall()
-        user = [cls.to_json(item) for item in users]
-        return user
+        try:
+            v2_db.cursor.execute("SELECT * FROM users")
+            users = v2_db.cursor.fetchall()
+            user = [cls.to_json(item) for item in users]
+            return user
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_by_field(cls, field, value):
         """Get a user from the database by key or field"""
-        query = "SELECT * FROM users WHERE {} = %s".format(field)
-        v2_db.cursor.execute(query, (value,))
-        users = v2_db.cursor.fetchall()
-        user = [cls.to_json(item) for item in users]
-        return user
+        try:
+            query = "SELECT * FROM users WHERE {} = %s".format(field)
+            v2_db.cursor.execute(query, (value,))
+            users = v2_db.cursor.fetchall()
+            user = [cls.to_json(item) for item in users]
+            return user
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_one_by_field(cls, field, value):
@@ -93,19 +99,25 @@ class Question(Question, DatabaseCollector):
     @classmethod
     def get_all(cls):
         """Get all questions from the database"""
-        v2_db.cursor.execute("SELECT * FROM questions")
-        questions = v2_db.cursor.fetchall()
-        question = [cls.to_json(quiz) for quiz in questions]
-        return question
+        try:
+            v2_db.cursor.execute("SELECT * FROM questions")
+            questions = v2_db.cursor.fetchall()
+            question = [cls.to_json(quiz) for quiz in questions]
+            return question
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_by_field(cls, field, value):
         """Get a user from the database by key or field"""
-        query = "SELECT * FROM questions WHERE {} = %s".format(field)
-        v2_db.cursor.execute(query, (value,))
-        questions = v2_db.cursor.fetchall()
-        question = [cls.to_json(item) for item in questions]
-        return question
+        try:
+            query = "SELECT * FROM questions WHERE {} = %s".format(field)
+            v2_db.cursor.execute(query, (value,))
+            questions = v2_db.cursor.fetchall()
+            question = [cls.to_json(item) for item in questions]
+            return question
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_one_by_field(cls, field, value):
@@ -117,17 +129,23 @@ class Question(Question, DatabaseCollector):
     @classmethod
     def delete(cls, _id):
         """deletes an item from the database"""
-        v2_db.cursor.execute("DELETE FROM questions WHERE id = %s", (_id,))
-        v2_db.connection.commit()
+        try:
+            v2_db.cursor.execute("DELETE FROM questions WHERE id = %s", (_id,))
+            v2_db.connection.commit()
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_item_by_id(cls, _id):
         """Retrieves an item by the id provided"""
-        v2_db.cursor.execute("SELECT * FROM questions WHERE id = %s", (_id,))
-        question = v2_db.cursor.fetchone()
-        if question is None:
-            return None
-        return cls.to_json(question)
+        try:
+            v2_db.cursor.execute("SELECT * FROM questions WHERE id = %s", (_id,))
+            question = v2_db.cursor.fetchone()
+            if question is None:
+                return None
+            return cls.to_json(question)
+        except Exception as e:
+            print(e)
 
     @classmethod
     def migrate(cls):
@@ -162,51 +180,66 @@ class Question(Question, DatabaseCollector):
 
     @classmethod
     def transform_for_search(cls):
-        v2_db.cursor.execute(
-            "SELECT title || '. ' || description as document, "
-            "to_tsvector(title || '. ' || description) as metadata "
-            "FROM questions"
-        )
+        try:
+            v2_db.cursor.execute(
+                "SELECT title || '. ' || description as document, "
+                "to_tsvector(title || '. ' || description) as metadata "
+                "FROM questions"
+            )
+        except Exception as e:
+            print(e)
 
     @classmethod
     def fts_search_query(cls, search_item):
-        v2_db.cursor.execute(
-            "SELECT * FROM questions "
-            "WHERE to_tsvector(title || '. ' || description) @@ "
-            "to_tsquery('{}')".format(search_item)
-        )
-        result = v2_db.cursor.fetchall()
-        items = [cls.to_json(item) for item in result]
-        return items
+        try:
+            v2_db.cursor.execute(
+                "SELECT * FROM questions "
+                "WHERE to_tsvector(title || '. ' || description) @@ "
+                "to_tsquery('{}')".format(search_item)
+            )
+            result = v2_db.cursor.fetchall()
+            items = [cls.to_json(item) for item in result]
+            return items
+        except Exception as e:
+            print(e)
 
     @classmethod
     def updatequestion(cls, answers, _id):
-        v2_db.cursor.execute(
-            "UPDATE questions SET answers = %s WHERE id = %s", (
-                answers,
-                _id
+        try:
+            v2_db.cursor.execute(
+                "UPDATE questions SET answers = %s WHERE id = %s", (
+                    answers,
+                    _id
+                )
             )
-        )
-        v2_db.connection.commit()
+            v2_db.connection.commit()
+        except Exception as e:
+            print(e)
 
 class Answer(Answer, DatabaseCollector):
     __table__ = "answers"
 
     @classmethod
     def get_all(cls):
-        v2_db.cursor.execute("SELECT * FROM answers")
-        answers = v2_db.cursor.fetchall()
-        answer = [cls.to_json(item) for item in answers]
-        return answer
+        try:
+            v2_db.cursor.execute("SELECT * FROM answers")
+            answers = v2_db.cursor.fetchall()
+            answer = [cls.to_json(item) for item in answers]
+            return answer
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_by_field(cls, field, value):
         """Get a user from the database by key or field"""
-        query = "SELECT * FROM answers WHERE {} = %s".format(field)
-        v2_db.cursor.execute(query, (value,))
-        answers = v2_db.cursor.fetchall()
-        answer = [cls.to_json(item) for item in answers]
-        return answer
+        try:
+            query = "SELECT * FROM answers WHERE {} = %s".format(field)
+            v2_db.cursor.execute(query, (value,))
+            answers = v2_db.cursor.fetchall()
+            answer = [cls.to_json(item) for item in answers]
+            return answer
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_one_by_field(cls, field, value):
@@ -249,23 +282,29 @@ class Answer(Answer, DatabaseCollector):
 
     @classmethod
     def accepteandupdate(cls, accepted, _id):
-        v2_db.cursor.execute(
-            "UPDATE answers SET accepted = %s WHERE id = %s", (
-                accepted,
-                _id
+        try:
+            v2_db.cursor.execute(
+                "UPDATE answers SET accepted = %s WHERE id = %s", (
+                    accepted,
+                    _id
+                )
             )
-        )
-        v2_db.connection.commit()
+            v2_db.connection.commit()
+        except Exception as e:
+            print(e)
 
     @classmethod
     def voteandupdate(cls, votes, _id):
-        v2_db.cursor.execute(
-            "UPDATE answers SET votes = %s WHERE id = %s", (
-                votes,
-                _id
+        try:
+            v2_db.cursor.execute(
+                "UPDATE answers SET votes = %s WHERE id = %s", (
+                    votes,
+                    _id
+                )
             )
-        )
-        v2_db.connection.commit()
+            v2_db.connection.commit()
+        except Exception as e:
+            print(e)
 
 class BlackList(DatabaseCollector):
     __table__ = "blacklist"
@@ -277,11 +316,14 @@ class BlackList(DatabaseCollector):
     @classmethod
     def get_by_field(cls, field, value):
         """Get a user from the database by key or field"""
-        query = "SELECT * FROM blacklist WHERE {} = %s".format(field)
-        v2_db.cursor.execute(query, (value,))
-        tokens = v2_db.cursor.fetchall()
-        token = [cls.to_json(item) for item in tokens]
-        return token
+        try:
+            query = "SELECT * FROM blacklist WHERE {} = %s".format(field)
+            v2_db.cursor.execute(query, (value,))
+            tokens = v2_db.cursor.fetchall()
+            token = [cls.to_json(item) for item in tokens]
+            return token
+        except Exception as e:
+            print(e)
 
     @classmethod
     def get_one_by_field(cls, field, value):
